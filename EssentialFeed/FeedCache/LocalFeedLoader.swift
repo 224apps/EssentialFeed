ef310ff8efc8f0c72ed36053a3552fe78e4e6b44
224Apps <224apps@gmail.com>
2021-06-20 15:38:23 -0400

    Load command delivers no images on seven days old cache

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -10,6 +10,7 @@ import Foundation
 public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
+    private let calendar = Calendar(identifier: .gregorian)
     
     public typealias SaveResult = Error?
     public typealias LoadResult = LoadFeedResult
@@ -31,17 +32,25 @@ public final class LocalFeedLoader {
     }
     
     public func load(completion: @escaping (LoadResult)-> Void){
-        store.retrieve { result in
+        store.retrieve { [unowned self] result in
             
             switch result {
                 case let .failure(error):
                     completion(.failure(error))
-                case .empty:
-                    completion(.success([]))
-                case let .found(feed, _):
+                case let .found(feed, timestamp) where self.validate(timestamp):
                     completion(.success(feed.toModels()))
+                case .found, .empty:
+                    completion(.success([]))
             }
         }
+    }
+    
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else{ return false }
+        return currentDate() < maxCacheAge
     }
     
     private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult)-> Void){
