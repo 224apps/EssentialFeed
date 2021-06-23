7bb32450f09907d5903d72efe3ff6d2cd141bf64
224Apps <224apps@gmail.com>
2021-06-23 09:32:51 -0400

    Dispatch CodableFeedStore operations in a serial background queue to avoid blocking clients

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -111,16 +111,6 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieve: .empty)
     }
     
-    func test_delete_deliversErrorOnDeletionError() {
-            let noDeletePermissionURL = cachesDirectory()
-            let sut = makeSUT(storeURL: noDeletePermissionURL)
-
-            let deletionError = deleteCache(from: sut)
-
-            XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
-            expect(sut, toRetrieve: .empty)
-        }
-    
     func test_storeSideEffects_runSerially(){
         let sut = makeSUT()
         
@@ -151,6 +141,19 @@ class CodableFeedStoreTests: XCTestCase {
         
     
     }
+    
+    func test_delete_deliversErrorOnDeletionError() {
+        let noDeletePermissionURL = cachesDirectory()
+        let sut = makeSUT(storeURL: noDeletePermissionURL)
+        
+        let deletionError = deleteCache(from: sut)
+        
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail")
+        expect(sut, toRetrieve: .empty)
+    }
+    
+    
+    
     
     // -MARK: - Helpers
     
@@ -215,8 +218,8 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     private func cachesDirectory() -> URL {
-            return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-        }
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
     
     private func setupEmptyStoreState(){
         deleteStoreArtifacts()
