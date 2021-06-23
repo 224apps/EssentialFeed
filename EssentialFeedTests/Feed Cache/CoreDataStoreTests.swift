2cd11d5d39572439a914292dacc74742c71e6dd3
224Apps <224apps@gmail.com>
2021-06-23 14:19:47 -0400

    CoreDataFeedStore.deleteCachedFeed() empties previously inserted cache

--- EssentialFeedTests/Feed Cache/CoreDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataStoreTests.swift
@@ -73,7 +73,9 @@ class CoreDataStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_delete_emptiesPreviouslyInsertedCache() {
-        
+        let sut = makeSUT()
+
+                assertThatDeleteEmptiesPreviouslyInsertedCache(on: sut)
     }
     
     func test_storeSideEffects_runSerially() {
