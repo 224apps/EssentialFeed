abc1fb1316c26b5f6ea3ec92e9e7f659389c104e
224Apps <224apps@gmail.com>
2021-06-25 15:13:26 -0400

    Decouple tests from specific UI controls for user initiated reloads wth a test-specific DSL method

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -52,30 +52,31 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
     
-    func test_pullToRefresh_loadsFeed(){
+    func test_userInitiatedFeedReload_reloadsFeed(){
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
         
-        sut.refreshControl?.simulatePullToRefresh()
+        sut.simulateUserInitiatedFeedReload()
         XCTAssertEqual(loader.loadCallCount, 2)
         
         sut.refreshControl?.simulatePullToRefresh()
         XCTAssertEqual(loader.loadCallCount, 3)
     }
     
-    func test_pullToRefresh_showsLoadingIndicator(){
+    func test_userInitiatedFeedReload_showsLoadingIndicator(){
         let (sut, _) = makeSUT()
         
-        sut.loadViewIfNeeded()
+       
+        sut.simulateUserInitiatedFeedReload()
         
         XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
     }
     
-    func test_pullToRefresh_hidesLoadingIndicatorOnLoaderCompletion(){
+    func test_userInitiatedFeedReload_hidesLoadingIndicatorOnLoaderCompletion(){
         let (sut, loader) = makeSUT()
         
-        sut.loadViewIfNeeded()
+        sut.simulateUserInitiatedFeedReload()
         loader.completeFeedLoading()
         
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
@@ -113,6 +114,11 @@ final class FeedViewControllerTests: XCTestCase {
     }
 }
 
+private extension FeedViewController {
+    func simulateUserInitiatedFeedReload(){
+        refreshControl?.simulatePullToRefresh()
+    }
+}
 
 private extension UIRefreshControl {
     func simulatePullToRefresh(){
