dff1f42a8cd1e8a8864f2b2c7d387cd077ac4c1b
224Apps <224apps@gmail.com>
2021-06-26 01:15:52 -0400

    Render loaded feed on successful load completion

---
+++ EssentialFeedTests/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeedTests
+//
+//  Created by Abdoulaye Diallo on 6/26/21.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
