211a90c613ac56284bdf01ac4d8567aa216cff5b
224Apps <224apps@gmail.com>
2021-06-30 15:40:06 -0400

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Abdoulaye Diallo on 6/30/21.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
